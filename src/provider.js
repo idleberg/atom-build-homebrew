45441bf0b2313e8f27209d9fb37b7827a56f3fd1
idleberg <git@idleberg.com>
2021-03-20 01:50:08 +0100

    remove duplicates

--- src/provider.js
+++ src/provider.js
@@ -7,26 +7,6 @@ import meta from '../package.json';
 
 export { configSchema as config };
 
-// Package settings
-import meta from '../package.json';
-
-export const config = {
-  manageDependencies: {
-    title: 'Manage Dependencies',
-    description: 'When enabled, third-party dependencies will be installed automatically',
-    type: 'boolean',
-    default: true,
-    order: 1
-  },
-  alwaysEligible: {
-    title: 'Always Eligible',
-    description: 'The build provider will be available in your project, even when not eligible',
-    type: 'boolean',
-    default: false,
-    order: 2
-  }
-};
-
 export function provideBuilder() {
   return class HomebrewProvider extends EventEmitter {
     constructor(cwd) {
